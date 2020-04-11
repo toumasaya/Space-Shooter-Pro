1af64f1a5e72cb50f005814028e13c21a819a095
toumasaya <luffy.w.pirate@gmail.com>
2020-04-11 20:59:38 +0800

    Create the player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float _speed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take the current position = new position (0, 0, 0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        float currentXPos = transform.position.x;
+        float currentYPos = transform.position.y;
+
+        transform.position = new Vector3(currentXPos, Mathf.Clamp(currentYPos, -3.8f, 0), 0);
+
+        if (transform.position.x > 11)
+        {
+            transform.position = new Vector3(-11, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
+        }
+    }
+}
